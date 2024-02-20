7ecc9a4b9b6acc554d736064d4ddcd56c6621140
b3ath <jakubwelky@seznam.cz>
2024-02-20 14:05:25 +0100

    ..

--- index.php
+++ index.php
@@ -1,50 +1,16 @@
-<?php
-    $notice = '';
-    $guessCount = 0;
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $userGuess = $_POST['guess'];
-        $guessCount++;
-
-        if (is_numeric($userGuess)) {
-            if ($userGuess < 1 || $userGuess > 100) {
-                $notice = 'Please enter a number between 1 and 100.';
-            } else {
-                $randomNumber = rand(1, 100);
-
-                if ($userGuess == $randomNumber) {
-                    $notice = 'Congratulations! You guessed the number.';
-                } else if ($userGuess < $randomNumber) {
-                    $notice = 'Your guess is too low.';
-                } else if ($userGuess > $randomNumber) {
-                    $notice = 'Your guess is too high.';
-                }
-            }
-        } else {
-            $notice = 'Please enter a valid number.';
-        }
-    }
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Guessing Game</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Number Guessing Game</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h1>Guess the Number</h1>
-    <p>We have selected a random number between 1 and 100. See if you can guess it in 10 turns or fewer. We'll tell you if your guess was too high or too low.</p>
-    <form id="guess-form" method="post">
-        <label for="guess-field">Enter a guess: </label>
-        <input type="text" id="guess-field" name="guess" class="guess-field">
-        <input type="submit" value="Submit guess" class="guess-submit">
-    </form>
-    <div id="result-panel" class="result-panel">
-        <p id="notice"> <?php echo $notice; ?> </p>
-        <p id="guess-count">Number of guesses: <?php echo $guessCount; ?></p>
-    </div>
+    <h1>Number Guessing Game</h1>
+    <p>Guess a number between 1 and 100:</p>
+    <input type="text" id="guessInput">
+    <button onclick="checkGuess()">Submit Guess</button>
+    <p id="message"></p>
+
     <script src="script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-let guessCount = 0;
-let noticeText = '';
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
 
-document.getElementById('guess-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    let userGuess = parseInt(document.getElementById('guess-field').value);
-    guessCount++;
+function checkGuess() {
+    const guess = parseInt(document.getElementById('guessInput').value);
+    attempts++;
 
-    if (isNaN(userGuess)) {
-        noticeText = 'Please enter a valid number.';
-    } else if (userGuess < 1 || userGuess > 100) {
-        noticeText = 'Please enter a number between 1 and 100.';
+    if (guess === randomNumber) {
+        document.getElementById('message').innerHTML = `Congratulations! You guessed the correct number in ${attempts} attempts.`;
+    } else if (guess < randomNumber) {
+        document.getElementById('message').innerHTML = 'Try a higher number.';
     } else {
-        if (userGuess === randomNumber) {
-            noticeText = 'Congratulations! You guessed the number.';
-        } else if (userGuess < randomNumber) {
-            noticeText = 'Your guess is too low.';
-        } else if (userGuess > randomNumber) {
-            noticeText = 'Your guess is too high.';
-        }
+        document.getElementById('message').innerHTML = 'Try a lower number.';
     }
-
-    document.getElementById('notice').textContent = noticeText;
-    document.getElementById('guess-count').textContent = 'Number of guesses: ' + guessCount;
-
-    document.getElementById('guess-field').value = '';
-});
+}
